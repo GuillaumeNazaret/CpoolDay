da5f74f79e22478da1d453fd9a6f9d8295259c40
GuillaumeNazaret <167294226+GuillaumeNazaret@users.noreply.github.com>
2024-04-17 09:42:33 +0000

    feat(ex01): add the my_print_ascii function

---
+++ my_print_ascii.c
@@ -0,0 +1,18 @@
+int print_char(char);
+
+void my_print_ascii(void)
+{
+    for(int i = 32; i < 127; i++)
+    {
+        print_char(i);
+    }
+}
+
+/*
+* int
+* void
+* char
+* char*
+*
+* fonction : type name (params)
+*/
