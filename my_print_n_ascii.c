b64858b01aa8f5bd78eff7babf4a274e4d2a9680
GuillaumeNazaret <167294226+GuillaumeNazaret@users.noreply.github.com>
2024-04-17 13:12:30 +0000

    adding my_print_n_ascii

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -3,8 +3,13 @@ int print_char(char);
 void my_print_n_ascii(int howMany)
 {
     int offset = 32;
-    for(int i = offset; i <= offset + howMany; i++)
+    for(int i = offset; i <= offset + howMany - 1; i++)
     {
         print_char(i);
     }
+}
+
+int main(void)
+{
+    my_print_n_ascii(10);
 }
